1ad896569507bf4c458dd8bc2e426131896555f6
Matt Clark <matt@mclarkdev.com>
2019-10-10 09:52:21 -0700

    Initial commit of a global key listener.

---
+++ src/main/java/com/mclarkdev/tools/KeyHook/KeyboardHook.java
@@ -0,0 +1,81 @@
+package com.mclarkdev.tools.KeyHook;
+
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.LRESULT;
+import com.sun.jna.platform.win32.WinDef.WPARAM;
+
+public class KeyboardHook {
+
+	public interface KeyListener {
+
+		public void onKeyUp(User32.KBDLLHOOKSTRUCT lParam);
+
+		public void onKeyDown(User32.KBDLLHOOKSTRUCT lParam);
+	}
+
+	private static KeyboardHook keyboardHook;
+
+	private KeyListener listener;
+
+	private User32.HHOOK hHook;
+
+	private KeyboardHook() {
+
+		(new Thread(runThread)).start();
+	}
+
+	public void setListener(KeyListener listener) {
+
+		this.listener = listener;
+	}
+
+	private User32.LowLevelKeyboardProc callback = new User32.LowLevelKeyboardProc() {
+
+		public LRESULT callback(int nCode, WPARAM wParam, User32.KBDLLHOOKSTRUCT lParam) {
+
+			switch (wParam.intValue()) {
+			case User32.WM_KEYUP:
+				listener.onKeyUp(lParam);
+				break;
+			case User32.WM_KEYDOWN:
+				listener.onKeyDown(lParam);
+				break;
+			}
+
+			return User32.INSTANCE.CallNextHookEx(hHook, nCode, wParam, null);
+		}
+	};
+
+	private Runnable runThread = new Runnable() {
+
+		public void run() {
+
+			// register the hook
+			KeyboardHook.this.hHook = User32.INSTANCE.SetWindowsHookEx(//
+					User32.WH_KEYBOARD_LL, callback, Kernel32.INSTANCE.GetModuleHandle(null), 0);
+
+			// look for messages forever
+			User32.MSG msg = new User32.MSG();
+			while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					Thread.sleep(1);
+					User32.INSTANCE.PeekMessage(msg, null, 0, 0, 0);
+				} catch (Exception e) {
+					break;
+				}
+			}
+		}
+	};
+
+	public static KeyboardHook getInstance() {
+
+		if (keyboardHook == null) {
+			keyboardHook = new KeyboardHook();
+		}
+
+		return keyboardHook;
+	}
+}
---
+++ src/main/java/com/mclarkdev/tools/KeyHook/KeyboardManager.java
@@ -0,0 +1,63 @@
+package com.mclarkdev.tools.KeyHook;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import com.mclarkdev.tools.KeyHook.KeyboardHook.KeyListener;
+import com.sun.jna.platform.win32.WinUser.KBDLLHOOKSTRUCT;
+
+public class KeyboardManager {
+
+	public interface EventListener {
+
+		public void onEvent(ArrayList<Integer> keyMap);
+	}
+
+	private static KeyboardManager keyboardManager;
+
+	private final HashSet<Integer> keysDown;
+
+	private EventListener listener;
+
+	private KeyboardManager() {
+
+		keysDown = new HashSet<>();
+		KeyboardHook.getInstance().setListener(keyListener);
+	}
+
+	public void setListener(EventListener listener) {
+
+		this.listener = listener;
+	}
+
+	private KeyListener keyListener = new KeyListener() {
+
+		public void onKeyUp(KBDLLHOOKSTRUCT event) {
+
+			synchronized (keysDown) {
+				keysDown.remove(Integer.valueOf(event.vkCode));
+			}
+		}
+
+		public void onKeyDown(KBDLLHOOKSTRUCT event) {
+
+			ArrayList<Integer> keys;
+			synchronized (keysDown) {
+
+				keysDown.add(Integer.valueOf(event.vkCode));
+				keys = new ArrayList<>(keysDown);
+			}
+
+			listener.onEvent(keys);
+		}
+	};
+
+	public static KeyboardManager getInstance() {
+
+		if (keyboardManager == null) {
+			keyboardManager = new KeyboardManager();
+		}
+
+		return keyboardManager;
+	}
+}
